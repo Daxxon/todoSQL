575f7e90ae55e2bc31a7abc47d558e7684e5f224
Daniel Waggoner <daxxon37@hotmail.com>
2017-08-29 15:16:03 -0400

    turing in advanced sql stuff assignment

--- todo.sql
+++ todo.sql
@@ -1,16 +1,32 @@
 DELETE FROM todos WHERE priority > 0;
-INSERT INTO todos (title, details, priority)
+INSERT INTO todos (title, details, priority, created_at, completed_at)
   VALUES
-    ('feed the trash', 'put the compost in the garbage disposal', 5),
-    ('take out the cats', 'Lather, Rinse, Repeat', 6),
-    ('go to the store', 'they said, it will be fine, they said', 2),
-    ('finish this assignment', 'is this too meta?', 9000),
-    ('flush the landry', 'put the lye in the choconauts', 1);
+    ('feed the trash', 'put the compost in the garbage disposal', 5, '2017-08-29 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('take out the cats', 'Lather, Rinse, Repeat', 3, '2017-08-29 10:44:49.565052', NULL),
+    ('go to the store', 'they said, it will be fine, they said', 2, '2017-08-29 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('finish this assignment', 'is this too meta?', 9000, '2017-08-29 10:44:49.565052', NULL),
+    ('flush the landry', 'put the lye in the choconauts', 1, '2017-08-29 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('kill the wabbit', 'put bugs bunny on the mantle', 4, '2017-08-29 10:44:49.565052', NULL),
+    ('test the eggs', 'you know, do stuff to make sure they"re whatever', 3, '2017-08-29 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('do stuff', 'also do things', 1, '2017-08-28 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('write 11 more todos', '11 after this one; 12 counting this one', 5, '2013-07-21 10:44:49.565052', NULL),
+    ('write 10 more todos', '11 after this one; 12 counting this one', 2, '2015-9-14 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('write 9 more todos', '11 after this one; 12 counting this one', 3, '2014-10-7 10:44:49.565052', NULL),
+    ('write 8 more todos', '11 after this one; 12 counting this one', 3, '2015-11-21 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('write 7 more todos', '11 after this one; 12 counting this one', 4, '2014-2-2 10:44:49.565052', NULL),
+    ('write 6 more todos', '11 after this one; 12 counting this one', 4, '2016-12-13 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('write 5 more todos', '11 after this one; 12 counting this one', 6, '2014-04-24 10:44:49.565052', NULL),
+    ('write 4 more todos', '11 after this one; 12 counting this one', 2, '2015-1-19 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('write 3 more todos', '11 after this one; 12 counting this one', 1, '2015-08-9 10:44:49.565052', NULL),
+    ('write 2 more todos', '11 after this one; 12 counting this one', 3, '2015-06-21 10:44:49.565052', '2017-08-29 10:44:49.565052'),
+    ('write 1 more todos', '11 after this one; 12 counting this one', 3, '2017-08-19 10:44:49.565052', NULL),
+    ('write NO more todos', 'NONE after this one; NONE counting this one', 6, '2016-08-28 10:44:49.565052', '2017-08-29 10:44:49.565052');
 
-SELECT FROM todos WHERE completed_at IS NULL;
+SELECT * FROM todos WHERE completed_at IS NULL AND priority = 3;
 
-SELECT FROM todos WHERE priority > 1;
+SELECT * FROM todos WHERE completed_at IS NULL ORDER BY priority DESC;
 
-UPDATE todos SET completed_at = NOW() WHERE id = 4;
+SELECT * FROM todos WHERE age(created_at) < '0 years 30 days';
 
-DELETE FROM todos WHERE completed_at IS NOT NULL;
+
+SELECT title, MAX(created_at), priority FROM todos GROUP BY title, priority ORDER BY MIN(priority) ASC LIMIT 1;
--- todos.sql
+++ todos.sql
@@ -7,3 +7,11 @@ CREATE TABLE todos (
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   completed_at TIMESTAMP
 );
+
+
+
+-- Write INSERT statements to insert 20 todos into the todos table, with a combination of priorities, created times, and completed times, with not all having a completed time.
+-- Write a SELECT statement to find all incomplete todos with priority 3.
+-- Write a SELECT statement to find the number of incomplete todos by priority.
+-- Write a SELECT statement to find the number of todos by priority created in the last 30 days.
+-- Write a SELECT statement to find the next todo you should work on. This is the todo with the highest priority that was created first.
